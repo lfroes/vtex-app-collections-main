967f48976a86f12dee0c358559e72ef26eefcbdf
lfroes <luiz.froes@completteweb.com>
2023-10-17 17:22:32 -0300

    fix: changed nomeclatures

---
+++ react/collectionsMain.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Layout from '@vtex/styleguide/lib/Layout'
+import PageBlock from '@vtex/styleguide/lib/PageBlock'
+
+
+const collectionsMain = () => {
+    return (
+        <PageBlock title="Collections Sellers" variation="aside">
+            <section></section>
+            <section></section>
+        </PageBlock>
+    )
+}
+
+export default collectionsMain
